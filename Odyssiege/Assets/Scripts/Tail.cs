29860c0f350f5eaf6f4bd5f359fc6a45fb57ec6f
druerridge <druerridge@gmail.com>
2018-03-15 14:04:41 -0700

    Tail moves sort of and is a prefab

--- Odyssiege/Assets/Scripts/Tail.cs
+++ Odyssiege/Assets/Scripts/Tail.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
 
 public class Tail : MonoBehaviour {
 
@@ -11,6 +12,11 @@ public class Tail : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            Debug.Log("Adding K");
+            gameObject.GetComponent<Rigidbody2D>().AddTorque(100.0f);
+        }
 		
 	}
 }
