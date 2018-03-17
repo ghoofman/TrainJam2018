1a0d157e5e660421129d79e122191f381f0e8567
Garrett Hoofman <gambitsunob@gmail.com>
2018-03-17 17:14:43 -0400

    Transition scenes

--- Odyssiege/Assets/Scripts/LevelEnd.cs
+++ Odyssiege/Assets/Scripts/LevelEnd.cs
@@ -4,11 +4,10 @@ using System.Collections;
 public class LevelEnd : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D coll) {
-		Debug.Log ("NEXT!");
 		if (coll.gameObject == Globals.horse.gameObject) {
 			// level has been completed
 			gameObject.GetComponent<BoxCollider2D>().enabled = false;
-			Globals.levelController.LoadNextLevel();
+			Globals.levelController.LoadTransition();
 		}
 	}
 }
---
+++ Odyssiege/Assets/TransitionFade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG;
+using DG.Tweening;
+
+public class TransitionFade : MonoBehaviour {
+
+	bool ending = false;
+	public bool CanRunAnimation = false;
+
+	// Use this for initialization
+	void Start () {
+		ending = false;
+		CanRunAnimation = false;
+
+		var spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		spriteRenderer.DOColor (new Color (0, 0, 0, 1.0f), 0.75f).OnComplete(() => {
+			Globals.levelController.BeginLoadingSequence();
+			CanRunAnimation = true;
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BeginEnd() {
+		if (ending)
+			return;
+		ending = true;
+		var spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		spriteRenderer.DOColor (new Color (0, 0, 0, 0.0f), 0.75f).OnComplete(() => {
+			Globals.levelController.FinishTransition();
+		});
+	}
+}
