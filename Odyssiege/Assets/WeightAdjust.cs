22fb5840a6b90619e68845a99f2159f8ce919738
Garrett Hoofman <gambitsunob@gmail.com>
2018-03-17 10:26:29 -0400

    Weight adjust on tail and head LT/RT

---
+++ Odyssiege/Assets/WeightAdjust.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class WeightAdjust : MonoBehaviour {
+	public int playerId = 0;
+	public Player player;
+	public string Trigger = "RT";
+
+	void Awake()
+	{
+		player = ReInput.players.GetPlayer(playerId);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float tempXAxis = player.GetAxis(Trigger);
+		GetComponent<Rigidbody2D> ().mass = 10.0f + (40.0f * tempXAxis);
+	}
+}
