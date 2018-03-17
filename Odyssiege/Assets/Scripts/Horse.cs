237d5d597fdb65d04686d52f9bb378b925df87eb
druerridge <druerridge@gmail.com>
2018-03-16 19:54:56 -0700

    Adding reset button and apparently a bunch of sound meta files

--- Odyssiege/Assets/Scripts/Horse.cs
+++ Odyssiege/Assets/Scripts/Horse.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Horse : MonoBehaviour {
 
@@ -16,47 +17,57 @@ public class Horse : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyUp(KeyCode.M))
         {
-            multiplayerEnabled = !multiplayerEnabled;
-            Debug.Log("multiplayer enabled: " + multiplayerEnabled);
-            if (multiplayerEnabled)
+            ToggleMultiplayer();
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    private void ToggleMultiplayer()
+    {
+        multiplayerEnabled = !multiplayerEnabled;
+        Debug.Log("multiplayer enabled: " + multiplayerEnabled);
+        if (multiplayerEnabled)
+        {
+            Debug.Log("multiplayer enabled2: " + multiplayerEnabled);
+
+            int playerId = 0;
+            var tails = GetComponentsInChildren<Tail>();
+            foreach (Tail tail in tails)
             {
-                Debug.Log("multiplayer enabled2: " + multiplayerEnabled);
-
-                int playerId = 0;
-                var tails = GetComponentsInChildren<Tail>();
-                foreach (Tail tail in tails)
-                {
-                    tail.playerId = playerId;
-                    tail.player = ReInput.players.GetPlayer(playerId);
-                    ++playerId;
-                }
-
-
-                var wheels = GetComponentsInChildren<Wheel>();
-                foreach (Wheel wheel in wheels)
-                {
-                    wheel.playerId = playerId;
-                    wheel.player = ReInput.players.GetPlayer(playerId);
-                    ++playerId;
-                }
+                tail.playerId = playerId;
+                tail.player = ReInput.players.GetPlayer(playerId);
+                ++playerId;
             }
-            else
+
+
+            var wheels = GetComponentsInChildren<Wheel>();
+            foreach (Wheel wheel in wheels)
             {
-                var tails = GetComponents<Tail>();
-                foreach (Tail tail in tails)
-                {
-                    tail.playerId = 0;
-                    tail.player = ReInput.players.GetPlayer(0);
-                }
-
-
-                var wheels = GetComponents<Wheel>();
-                foreach (Wheel wheel in wheels)
-                {
-                    wheel.playerId = 0;
-                    wheel.player = ReInput.players.GetPlayer(0);
-                }
+                wheel.playerId = playerId;
+                wheel.player = ReInput.players.GetPlayer(playerId);
+                ++playerId;
             }
         }
-	}
+        else
+        {
+            var tails = GetComponents<Tail>();
+            foreach (Tail tail in tails)
+            {
+                tail.playerId = 0;
+                tail.player = ReInput.players.GetPlayer(0);
+            }
+
+
+            var wheels = GetComponents<Wheel>();
+            foreach (Wheel wheel in wheels)
+            {
+                wheel.playerId = 0;
+                wheel.player = ReInput.players.GetPlayer(0);
+            }
+        }
+    }
 }
